70d4f8212851de98f8ed464a48709e03e5d0787b
MIMMI NILSSON <mimmizlnilsson@gmail.com>
2022-03-08 12:53:24 +0100

    test-staging

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import React, {useEffect, useState, createContext} from 'react';
 import {BrowserRouter, Routes, Route,} from "react-router-dom";
 import Header from './components/Header';
-import Homepage from './pages/Homepage';
-import Products from './pages/Products';
+import HomePage from "./components/Pages/HomePage";
+import Products from "./components/Pages/Products";
 import Product from './pages/Product';
 import Checkout from './pages/Checkout';
 import Footer from './components/Footer';
@@ -13,66 +13,7 @@ export const AppContext = createContext();
 
 function App() {
 
-  //==========CART / ADD ITEMS==========
-  const [cartItems, setCartItems] = useState([]);
-  
-  const addToCart = (newItem) => {
-    console.log(newItem)
-    setCartItems([
-      ...cartItems, 
-      newItem
-    ])
-  }
-
-  //==========(CHECKOUT) / TOTAL SUM==========
-  const [checkoutTotal, setCheckoutTotal] = useState(0);
-  
-  const total = () => {
-    let totalValue = 0;
-    for (let i = 0; i < cartItems.length; i++) {
-      totalValue += cartItems[i].price; 
-    }
-    setCheckoutTotal(totalValue);
-  }
-
-  useEffect(() => {
-    total();
-  }, [cartItems]);
-                
-  return (     
-    <AppContext.Provider value = {{
-      setCartItems,
-      cartItems
-    }}>            
-    <div className="page-container">  
-      <div className='content-wrap'>
-        <BrowserRouter>
-        <Header />
-          <Routes>
-            <Route path='/home' element={<Homepage cartItems={cartItems}/>}></Route>
-            <Route path='/products' element={<Products addToCart={addToCart}/>}></Route>
-            <Route path='/products/:id' element={<Product addToCart={addToCart} cartItems={cartItems}/>}></Route>
-            <Route path='/checkout' element={<Checkout setCartItems={setCartItems} addToCart={addToCart} cartItems={cartItems} checkoutTotal={checkoutTotal} />}></Route>
-            <Route path='/checkoutmsg' element={<CheckoutMsg />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-      <Footer />
-    </div>
-    </AppContext.Provider>  
-  );
-}
-
-export default App;
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Products from "./components/Pages/Products";
-import HomePage from "./components/Pages/HomePage";
-import Header from "./components/Header";
-import React, { useEffect, useState } from "react";
-
-function App() {
-
+  //==========ERIK==========
   const [items, setItems] = useState([]);
 
   const [sum, setSum] = useState(0); 
@@ -106,39 +47,71 @@ function App() {
     setSum(totalVal)
   }
 
-  const addToCart = (newItem) => {
-    console.log(newItem);
-    setItems([...items, newItem]);
-
-  };
-
   const deleteFromCart = (id) => {
     let remove = items.filter((item) => item.id !== id);
     setItems(remove);
   };
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header
-          addToCart={addToCart}
-          items={items}
-          deleteFromCart={deleteFromCart}
-          setItems={setItems} 
-          sum={sum} 
-          setSum={setSum}
-          quantity={quantity}
-          setQuantity={setQuantity}
-        />
-        <Routes>
-          <Route
+  //==========CART / ADD ITEMS==========
+  const [cartItems, setCartItems] = useState([]);
+  
+  const addToCart = (newItem) => {
+    console.log(newItem)
+    setCartItems([
+      ...cartItems, 
+      newItem
+    ])
+  }
+
+  //==========(CHECKOUT) / TOTAL SUM==========
+  const [checkoutTotal, setCheckoutTotal] = useState(0);
+  
+  const totalCheckout = () => {
+    let totalValue = 0;
+    for (let i = 0; i < cartItems.length; i++) {
+      totalValue += cartItems[i].price; 
+    }
+    setCheckoutTotal(totalValue);
+  }
+
+  useEffect(() => {
+    total();
+  }, [cartItems]);
+                
+  return (     
+    <AppContext.Provider value = {{
+      setCartItems,
+      cartItems
+    }}>            
+    <div className="page-container">  
+      <div className='content-wrap'>
+        <BrowserRouter>
+          <Header
+            addToCart={addToCart}
+            items={items}
+            deleteFromCart={deleteFromCart}
+            setItems={setItems} 
+            sum={sum} 
+            setSum={setSum}
+            quantity={quantity}
+            setQuantity={setQuantity}
+          />
+          <Routes>
+            <Route path="/Home" element={<HomePage />}></Route>
+            <Route
             path="/Products"
-            element={<Products addToCart={addToCart} />}
-          ></Route>
-          <Route path="/Home" element={<HomePage />}></Route>
-        </Routes>
-      </BrowserRouter>
+            element={<Products 
+            addToCart={addToCart} />} >
+            </Route>            
+            <Route path='/products/:id' element={<Product addToCart={addToCart} cartItems={cartItems}/>}></Route>
+            <Route path='/checkout' element={<Checkout setCartItems={setCartItems} addToCart={addToCart} cartItems={cartItems} checkoutTotal={checkoutTotal} />}></Route>
+            <Route path='/checkoutmsg' element={<CheckoutMsg />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
     </div>
+    </AppContext.Provider>  
   );
 }
 
